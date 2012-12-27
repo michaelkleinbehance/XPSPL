2cd99a31a052fd19aab0487c432b9467de580ff8
prggmr ♠ <prggmr@gmail.com>
2012-12-26 23:11:33 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -40,7 +40,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'prggmr'
+project = u'XPSPL'
 copyright = u'2012, Nickolas Whiting'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -164,7 +164,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'prggmrdoc'
+htmlhelp_basename = 'xpspldoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -183,7 +183,7 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'prggmr.tex', u'prggmr Documentation',
+  ('index', 'xpspl.tex', u'xpspl Documentation',
    u'Nickolas Whiting', 'manual'),
 ]
 
@@ -213,7 +213,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'prggmr', u'prggmr Documentation',
+    ('index', 'xpspl', u'XPSPL Documentation',
      [u'Nickolas Whiting'], 1)
 ]
 
@@ -227,8 +227,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'prggmr', u'prggmr Documentation',
-   u'Nickolas Whiting', 'prggmr', 'One line description of project.',
+  ('index', 'xpspl', u'XPSPL Documentation',
+   u'Nickolas Whiting', 'XPSPL', 'One line description of project.',
    'Miscellaneous'),
 ]
 
